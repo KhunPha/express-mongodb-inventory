50c49c23a5b87ad066b95d75d3db5dc1130769a4
KhunPha <sophadaro2016@gmail.com>
2024-01-02 23:20:37 +0700

    now

--- src/routes/AuthRoute.js
+++ src/routes/AuthRoute.js
@@ -3,5 +3,10 @@ const app = express()
 const AuthController = require("../controller/AuthController")
 
 app.post("/register", AuthController.register)
+app.get("/", (req, res)=>{
+    res.json({
+        message: "Hello World"
+    })
+})
 
 module.exports = app
